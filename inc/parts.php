3691481fc48ba54118b926a05cf6bd0a28eeade4
Tiberius Maximilian <tiberius.maximilian@mt2015.com>
2018-01-30 17:13:45 +0100

    My start

---
+++ inc/parts.php
@@ -0,0 +1,7 @@
+<?php
+function print_headers(){    ?>
+<head>
+<link rel="stylesheet" href="lilith.css" />
+</head>
+<?php 
+};
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'inc/parts.php';
+?>
+<html>
+<?php print_headers()?>
+<body>
+<?php print 'hei '.rand(1,42); ?>
+</body>
+</html>
