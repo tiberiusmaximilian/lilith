947518d42251ef3176136c1bc532c817851bd869
Tiberius Maximilian <tiberius.maximilian@mt2015.com>
2018-02-08 12:21:44 +0100

    setup started, removing directory, adding directory and pipe

--- inc/setup.php
+++ inc/setup.php
@@ -1,2 +1,58 @@
 <?php
-$log[]='setup.php';
+$log[] = 'setup.php';
+
+function removeDir($name){
+   $handle=opendir($name);
+   /* This is the correct way to loop over the directory. */
+   while (false !== ($entry = readdir($handle))) {
+       if($entry[0]=='.')
+           continue;
+       $entry=$name.'/'.$entry;
+       echo $entry.' ';
+       if(is_dir($entry)){
+           removeDir($entry);
+       }else {
+           unlink($entry);
+       }
+       
+   }
+   closedir($handle);
+   rmdir($name);
+}
+
+function makeData()
+{
+    global $dataDir;
+    echo '<ol>';
+    echo '<li>'.$dataDir.' directory</li>';
+    if (is_dir($dataDir)) {
+        echo '<li>data directory exists</li>';
+        removeDir($dataDir);
+        
+    } 
+    
+    {
+        mkdir($dataDir, 0777, false) || die('no directory');
+        echo '<li>data directory created</li>';
+    }
+    if (function_exists('posix_mkfifo')) {
+        echo '<li>creating fifo</li>';
+        posix_mkfifo('data/pipe', 0777) || die('couldn\'t create pipe');
+        echo '<li>pipe created</li>';
+    } else {
+        echo '<li>function for creating pipe does not exist - install the posix extensions</li>';
+    }
+    
+    echo '</ol>';
+}
+
+function doSetup()
+{
+    global $adminFile;
+    if (isset($_POST['adminName'])) {
+        makeData();
+        file_put_contents($adminFile, $_POST['adminName']);
+    } else {
+        echo '<div id="centerbox"><h4>Enter the nickname of the first admin</h4><form method="post">' . '<input type="text" name="adminName" /></form> </div>';
+    }
+}
--- index.php
+++ index.php
@@ -6,23 +6,28 @@ require_once 'inc/parts.php';
 echo '<head>';
 print_headers();
 echo '</head><body>';
+$dataDir='data';
 if (isset($_SESSION['nick'])) {
     // go on with the usual interaction
 } else {
     $log[] = 'no nick in session';
     // login?
-    $adminFile = 'admin.txt';
+    $adminFile = 'data/admin.txt';
     if (file_exists($filename)) {
         // written, hence login
+        $log[] = 'got a designated admin';
     } else {
         $log[] = 'going setup';
         require_once 'inc/setup.php';
+        doSetup();
     }
 }
-// debug the log
-echo '<div id="log"><h5>Log:</h5><ol>';
-foreach($log as $key => $val)
-    echo '<li>'. $val .'</li>';
-echo '</ol></div>';
+{
+    // debug the log
+    echo '<div id="log"><h5>Log:</h5><ol>';
+    foreach ($log as $key => $val)
+        echo '<li>' . $val . '</li>';
+    echo '</ol></div>';
+}
 echo '</body></html>';
 
